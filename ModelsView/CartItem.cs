eac885038ede2a773b5e1402998560234f7452d1
Chan Gia Hoang <lytieuman.workspace2025@gmail.com>
2025-11-05 17:33:45 +0700

    Dev

---
+++ ModelsView/CartItem.cs
@@ -0,0 +1,12 @@
+﻿
+using Pet_Shop2.Models;
+
+namespace Pet_Shop2.ModelsView
+{
+    public class CartItem
+    {
+        public Product? Product { get; set; }
+        public int amount { get; set; }
+        public double TotalMoney => amount * (Product.Price.HasValue ? (double)Product.Price : 0.0);
+    }
+}
